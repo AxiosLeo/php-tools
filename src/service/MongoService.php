c8cecd35186701d4d2d0d1b62ae85174989b686b
Axios <axioscros@aliyun.com>
2017-05-05 15:25:09 +0800

    MongoService 支持多连接配置

--- src/service/MongoService.php
+++ src/service/MongoService.php
@@ -7,12 +7,28 @@
  * @datetime: 2017/4/10 15:59
  */
 namespace axios\tpr\service;
+
 use think\Config;
 use think\Db;
 
+/**
+ * Class MongoService
+ * @package axios\tpr\service
+ */
 class MongoService{
+    public static $config = [];
+    public static function connect($select='default'){
+        self::$config = Config::get('mongo.'.$select);
+        return new self();
+    }
     public static function name($name=''){
-        $config = Config::get('mongo');
-        return Db::connect($config)->name($name);
+        if(empty(self::$config)){
+            self::$config =  Config::get('mongo.default');
+        }
+        return Db::connect(self::$config )->name($name);
+    }
+    public function __call($name, $arguments)
+    {
+        return Db::connect(self::$config )->name($arguments);
     }
 }
