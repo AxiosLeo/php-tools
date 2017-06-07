2bcdc42e04a3666a0a7d5ddcd64de0732e6b0671
Axios <axioscros@aliyun.com>
2017-06-07 13:49:27 +0800

    2.0.2

--- example/app/index/controller/Test.php
+++ example/app/index/controller/Test.php
@@ -13,6 +13,7 @@ namespace tpr\index\controller;
 
 use axios\tpr\core\Api;
 use axios\tpr\service\EnvService;
+use axios\tpr\service\MongoService;
 
 class Test extends Api{
     public function index(){
@@ -21,21 +22,17 @@ class Test extends Api{
     public function env(){
         $env_dir = ROOT_PATH.".env2";
         dump($env_dir);
-//        EnvService::select($env_dir);
-//        $env = EnvService::all();
-//        dump($env);
-//        $env = EnvService::all(false);
-//        dump($env);
-//        EnvService::select($env_dir);
-//        EnvService::select($env_dir);
         EnvService::select($env_dir);
         dump(EnvService::all());
-//        dump(EnvService::all(false));
         dump(EnvService::get('test.test.0'));
         dump(EnvService::set('test.test.0',time()));
         dump(EnvService::get('test.test.0'));
         dump(EnvService::all());
         dump(EnvService::save());
-//        dump(EnvService::all(false));
+    }
+    public function mongo(){
+        $result = MongoService::checkConnect();
+        dump($result);
+        dump(MongoService::$errorMsg);
     }
 }
