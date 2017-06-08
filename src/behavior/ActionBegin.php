cfb2dd6ea8331289a9052914b5b00366cdcef6d8
Axios <axioscros@aliyun.com>
2017-06-08 14:45:58 +0800

    update

--- src/behavior/ActionBegin.php
+++ src/behavior/ActionBegin.php
@@ -17,6 +17,7 @@ use axios\tpr\core\Result;
 use think\Validate;
 use think\Request;
 use think\Loader;
+use think\Env;
 
 class ActionBegin{
     public $param;
@@ -34,6 +35,7 @@ class ActionBegin{
     }
 
     public function run(){
+        $this->sign();
         $class = Loader::parseClass($this->module, 'validate',$this->controller,false);
         if(class_exists($class)){
             $Validate = Loader::validate($this->controller, 'validate', false,$this->module);
@@ -42,6 +44,44 @@ class ActionBegin{
 
         $this->middleware();
         $this->cache();
+    }
+
+    public function sign(){
+        $status = Env::get('sign.status');
+
+        if($status){
+            $timestamp = Env::get('sign.timestamp_name');
+            if(empty($timestamp)){
+                $timestamp = 'timestamp';
+            }
+            if(!isset($this->param[$timestamp])){
+                Result::wrong(401,$timestamp.' param not exits');
+            }
+            $timestamp = $this->param[$timestamp];
+
+            $expire = Env::get('sign.expire');
+            if(empty($expire)){
+                $expire = 10;
+            }
+            if(time()-intval($timestamp) > intval($expire)){
+                Result::wrong(401,md5($timestamp."azXCz5AEabA1Y9XhB").'sign timeout'.time());
+            }
+
+            $sign_name = Env::get('sign.sign_mame');
+            if(empty($sign_name)){
+                $sign_name = 'sign';
+            }
+            if(!isset($this->param[$sign_name])){
+                Result::wrong(401,$sign_name.' param not exits');
+            }
+
+            $sign = $this->param[$sign_name];
+            $sign_result = check_sign($timestamp,$sign);
+
+            if($sign_result!==true){
+                Result::wrong(401,'wrong sign');
+            }
+        }
     }
 
     /**
--- src/behavior/AppInit.php
+++ src/behavior/AppInit.php
@@ -27,7 +27,6 @@ class AppInit {
 
     public function run(){
         $this->lang();
-        $this->sign();
     }
 
     public function lang(){
@@ -38,50 +37,5 @@ class AppInit {
         }
         Lang::load($lang_path. $this->request->langset() . EXT);
     }
-
-    public function sign(){
-        $status = Env::get('sign.status');
-
-        if($status){
-            $timestamp = Env::get('sign.timestamp_name');
-            if(empty($timestamp)){
-                $timestamp = 'timestamp';
-            }
-            if(!isset($this->param[$timestamp])){
-                Result::wrong(401,$timestamp.' param not exits');
-            }
-            $timestamp = $this->param[$timestamp];
-
-            $expire = Env::get('sign.expire');
-            if(empty($expire)){
-                $expire = 10;
-            }
-            if(time()-intval($timestamp) > intval($expire)){
-                Result::wrong(401,md5($timestamp."azXCz5AEabA1Y9XhB").'sign timeout'.time());
-            }
-
-            $sign_name = Env::get('sign.sign_mame');
-            if(empty($sign_name)){
-                $sign_name = 'sign';
-            }
-            if(!isset($this->param[$sign_name])){
-                Result::wrong(401,$sign_name.' param not exits');
-            }
-
-            $sign = $this->param[$sign_name];
-            $sign_result = check_sign($timestamp,$sign);
-
-            if($sign_result!==true){
-                Result::wrong(401,'wrong sign');
-            }
-        }
-    }
-    public function __destruct()
-    {
-        // TODO: Implement __destruct() method.
-        unset($this->request);
-        unset($this->param);
-    }
-
 
 }
