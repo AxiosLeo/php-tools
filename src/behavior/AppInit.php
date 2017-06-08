e4d24ebe7883aa0a2512e715b69bc94130fe8b78
Axios <axioscros@aliyun.com>
2017-06-08 14:42:03 +0800

    AppInit加载公共语言包

--- src/behavior/AppInit.php
+++ src/behavior/AppInit.php
@@ -12,6 +12,7 @@
 namespace axios\tpr\behavior;
 
 use think\Env;
+use think\Lang;
 use think\Request;
 use axios\tpr\core\Result;
 
@@ -25,7 +26,17 @@ class AppInit {
     }
 
     public function run(){
+        $this->lang();
         $this->sign();
+    }
+
+    public function lang(){
+        $lang_path = defined('LANG_PATH')?LANG_PATH:CONF_PATH.'lang'.DIRECTORY_SEPARATOR;
+        $lastString = substr($lang_path,-1);
+        if($lastString!=DS){
+            $lang_path .= DS;
+        }
+        Lang::load($lang_path. $this->request->langset() . EXT);
     }
 
     public function sign(){
