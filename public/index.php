7af39c1931c1cb64184023da13efd04386f4473a
Axios <axioscros@aliyun.com>
2017-06-13 10:19:07 +0800

    update

--- public/index.php
+++ public/index.php
@@ -8,6 +8,19 @@ define('APP_PATH', ROOT_PATH . 'example/app/');
 define('CONF_PATH', ROOT_PATH.'example/config/');
 define('RUNTIME_PATH', ROOT_PATH . 'example/runtime/');
 define('APP_NAMESPACE','tpr');
+define('THINK_PATH',ROOT_PATH."vendor/topthink/framework/");
+
+// 加载Behavior
+require_once THINK_PATH."library/think/Hook.php";
+\think\Hook::add('app_init' ,'axios\\tpr\\behavior\\AppInit');
+\think\Hook::add('action_begin' ,'axios\\tpr\\behavior\\ActionBegin');
+\think\Hook::add('app_end' ,'axios\\tpr\\behavior\\AppEnd');
+
+// 加载公共语言包路径
+define('LANG_PATH',CONF_PATH.'lang/');
+
+//引入公共方法文件
+include_once CONF_PATH.'common.php';
 
 // 加载框架引导文件
-require __DIR__ . '/../vendor/topthink/framework/start.php';
+require THINK_PATH.'start.php';
