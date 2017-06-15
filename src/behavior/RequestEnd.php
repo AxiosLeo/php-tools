e0dac99b9fc8a4b0d9b06dd05f3249adb7a5a9ca
Axios <axioscros@aliyun.com>
2017-06-15 11:26:13 +0800

    request_end行为扩展

---
+++ src/behavior/RequestEnd.php
@@ -0,0 +1,29 @@
+<?php
+// +----------------------------------------------------------------------
+// | TPR [ Design For Api Develop ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017-2017 http://hanxv.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: axios <axioscros@aliyun.com>
+// +----------------------------------------------------------------------
+
+namespace axios\tpr\behavior;
+
+use think\Request;
+
+class RequestEnd{
+    public $param;
+    public $request;
+    function __construct()
+    {
+        $this->request = Request::instance();
+        $this->param = $this->request->param();
+    }
+
+    public function run(){
+        posix_kill(posix_getpid(), SIGINT);
+        exit();
+    }
+}
