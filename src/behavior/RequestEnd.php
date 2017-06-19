1bc2c4876315e55b2afd2d06f16116de163e7f9a
Axios <axioscros@aliyun.com>
2017-06-19 15:27:47 +0800

    行为监听

--- src/behavior/RequestEnd.php
+++ src/behavior/RequestEnd.php
@@ -11,12 +11,10 @@
 
 namespace axios\tpr\behavior;
 
-use axios\tpr\service\ToolService;
-use think\Lang;
+use axios\tpr\service\ForkService;
 use think\Request;
-use think\Hook;
 
-class AppInit {
+class RequestEnd extends ForkService{
     public $param;
     public $request;
     function __construct()
@@ -26,23 +24,7 @@ class AppInit {
     }
 
     public function run(){
-        Hook::add('action_begin' ,'axios\\tpr\\behavior\\ActionBegin');
-        Hook::add('app_end' ,'axios\\tpr\\behavior\\AppEnd');
-        Hook::add('log_write_done', 'axios\\tpr\\behavior\\LogWriteDone');
-        Hook::add('request_end', 'axios\\tpr\\behavior\\RequestEnd');
-        ToolService::identity(1);
-        $this->lang();
+        posix_kill(posix_getpid(), SIGINT);
+        exit();
     }
-
-    public function lang(){
-        Lang::detect();
-        $this->request->langset(Lang::range());
-        $lang_path = defined('LANG_PATH')?LANG_PATH:CONF_PATH.'lang'.DIRECTORY_SEPARATOR;
-        $lastString = substr($lang_path,-1);
-        if($lastString!=DS){
-            $lang_path .= DS;
-        }
-        Lang::load($lang_path. $this->request->langset() . EXT);
-    }
-
 }
