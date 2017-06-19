7d412ca4ba48082fa1afc777976b61ddf1a4ac2e
Axios <axioscros@aliyun.com>
2017-06-19 15:28:05 +0800

    update

--- example/app/index/controller/Index.php
+++ example/app/index/controller/Index.php
@@ -12,11 +12,10 @@ use axios\tpr\core\Api;
 use axios\tpr\service\ForkService;
 use think\Log;
 use tpr\index\service\TestService;
-
+use think\Session;
 class Index extends Api {
     public function index(){
         $test = new TestService();
-//        $test->test(1,2);
         for ($i=0;$i<15;$i++){
             ForkService::work($test,'test',[$i,2*$i+1]);
         }
