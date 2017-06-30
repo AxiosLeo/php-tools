2ab627d9d02e22384df216128f34249c21cc2b17
Axios <axioscros@aliyun.com>
2017-06-30 10:16:32 +0800

    update

--- example/app/index/controller/Index.php
+++ example/app/index/controller/Index.php
@@ -9,19 +9,31 @@
 namespace example\index\controller;
 
 use axios\tpr\core\Api;
+use axios\tpr\service\ApiDocService;
 use axios\tpr\service\ForkService;
 
 class Index extends Api {
     /**
-     * 多行注释
-     * @desc 一行写不下;
-     * @desc 那就两行
+     * 测试接口
+     * @desc
+     * @method post | get
+     * @parameter 参数类型 参数名称
+     * @response string param_name param_info description
+     * @a this is a
+     * @b this is b
+     * @array this is array 1
+     * @array this is array 2
+     * @multiRow this is ;
+     * @multiRow multiRow comment
+     * @multiRowParam string multi_row_param this is ;
+     * @multiRowParam multi_row_param comment
+     * @multiRowParam string multi_row;
+     * @multiRowParam _param2 this is multi_row_param comment
+     * @test void|string asdf
      */
     public function index(){
-//        $Test = new \example\index\service\Test();
-//        for($i = 0; $i<10 ; $i++){
-//            ForkService::work($Test,'files',[]);
-//        }
+        ApiDocService::config(__DIR__);
+        dump(ApiDocService::makeClassDoc(__CLASS__));
         $this->response('hello,world!');
     }
 
