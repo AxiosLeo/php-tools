04ff17adb1932825a89f94d0b8f65bc090907f99
axios <axiosleo@foxmail.com>
2021-02-22 16:41:52 +0800

    feat: sm3

--- src/SM3.php
+++ src/SM3.php
@@ -7,35 +7,176 @@ namespace axios\tools;
 /**
  * Class SM3.
  *
- * @reference https://www.fengkx.top/post/sm3-implementing/
+ * @reference https://github.com/lizhichao/sm
  */
 class SM3
 {
-    const IV = '7380166f4914b2b9172442d7da8a0600a96f30bc163138aae38dee4db0fb0e4e';
+    const IV      = '7380166f4914b2b9172442d7da8a0600a96f30bc163138aae38dee4db0fb0e4e';
+    const LEN     = 512;
+    const STR_LEN = 64;
 
-    private string $value = '';
+    private ?string $hex    = null;
+    private ?string $binary = null;
+    private ?string $base64 = null;
 
-    public function __construct($str)
+    public function encode(string $str): self
     {
-        $data = (string) $str;
+        $l   = strlen($str) * 8;
+        $k   = $this->getK($l);
+        $bt  = $this->getB($k);
+        $str = $str . $bt . pack('J', $l);
+
+        $count = strlen($str);
+        $l     = $count / self::STR_LEN;
+        $vr    = hex2bin(self::IV);
+        for ($i = 0; $i < $l; ++$i) {
+            $vr = $this->cf($vr, substr($str, $i * self::STR_LEN, self::STR_LEN));
+        }
+        $this->binary = $vr;
+
+        return $this;
     }
 
-    public function getValue(): string
+    public function encodeFile(string $file): self
     {
-        return $this->value;
+        $l  = filesize($file) * 8;
+        $k  = $this->getK($l);
+        $bt = $this->getB($k) . pack('J', $l);
+
+        $hd  = fopen($file, 'r');
+        $vr  = hex2bin(self::IV);
+        $str = fread($hd, self::STR_LEN);
+        if ($l > self::LEN - self::STR_LEN - 1) {
+            do {
+                $vr  = $this->cf($vr, $str);
+                $str = fread($hd, self::STR_LEN);
+            } while (!feof($hd));
+        }
+
+        $str   = $str . $bt;
+        $count = strlen($str) * 8;
+        $l     = $count / self::LEN;
+        for ($i = 0; $i < $l; ++$i) {
+            $vr = $this->cf($vr, substr($str, $i * self::STR_LEN, self::STR_LEN));
+        }
+        $this->binary = $vr;
+
+        return $this;
     }
 
-    public function getBase64Encode(): string
+    public function getBinary(): string
     {
-        return base64_encode($this->value);
+        return $this->binary;
     }
 
-    public function getHexEncode()
+    public function getBase64(): string
     {
-        return bin2hex($this->value);
+        if (null === $this->base64) {
+            $this->base64 = base64_encode($this->binary);
+        }
+
+        return $this->base64;
     }
 
-    private function padding()
+    public function getHex(): string
     {
+        if (null === $this->hex) {
+            $this->hex = bin2hex($this->getBinary());
+        }
+
+        return $this->hex;
+    }
+
+    private function getK($l): int
+    {
+        $v = $l % self::LEN;
+
+        return $v + self::STR_LEN < self::LEN
+            ? self::LEN - self::STR_LEN - $v - 1
+            : (self::LEN * 2) - self::STR_LEN - $v - 1;
+    }
+
+    private function getB($k): string
+    {
+        $arg = [128];
+        $arg = array_merge($arg, array_fill(0, (int) ($k / 8), 0));
+        $res = pack('C*', ...$arg);
+        if (false === $res) {
+            $res = '';
+        }
+
+        return $res;
+    }
+
+    private function t($i): int
+    {
+        return $i < 16 ? 0x79cc4519 : 0x7a879d8a;
+    }
+
+    private function cf($ai, $bi)
+    {
+        $wr = array_values(unpack('N*', $bi));
+        for ($i = 16; $i < 68; ++$i) {
+            $wr[$i] = $this->p1($wr[$i - 16]
+                    ^
+                    $wr[$i - 9]
+                    ^
+                    $this->lm($wr[$i - 3], 15))
+                ^
+                $this->lm($wr[$i - 13], 7)
+                ^
+                $wr[$i - 6];
+        }
+        $wr1 = [];
+        for ($i = 0; $i < 64; ++$i) {
+            $wr1[] = $wr[$i] ^ $wr[$i + 4];
+        }
+
+        list($a, $b, $c, $d, $e, $f, $g, $h) = array_values(unpack('N*', $ai));
+
+        for ($i = 0; $i < 64; ++$i) {
+            $ss1 = $this->lm(
+                ($this->lm($a, 12) + $e + $this->lm($this->t($i), $i % 32) & 0xffffffff),
+                7
+            );
+            $ss2 = $ss1 ^ $this->lm($a, 12);
+            $tt1 = ($this->ff($i, $a, $b, $c) + $d + $ss2 + $wr1[$i]) & 0xffffffff;
+            $tt2 = ($this->gg($i, $e, $f, $g) + $h + $ss1 + $wr[$i]) & 0xffffffff;
+            $d   = $c;
+            $c   = $this->lm($b, 9);
+            $b   = $a;
+            $a   = $tt1;
+            $h   = $g;
+            $g   = $this->lm($f, 19);
+            $f   = $e;
+            $e   = $this->p0($tt2);
+        }
+
+        return pack('N*', $a, $b, $c, $d, $e, $f, $g, $h) ^ $ai;
+    }
+
+    private function ff($j, $x, $y, $z): int
+    {
+        return $j < 16 ? $x ^ $y ^ $z : ($x & $y) | ($x & $z) | ($y & $z);
+    }
+
+    private function gg($j, $x, $y, $z): int
+    {
+        return $j < 16 ? $x ^ $y ^ $z : ($x & $y) | (~$x & $z);
+    }
+
+    private function lm($a, $n): int
+    {
+        return $a >> (32 - $n) | (($a << $n) & 0xffffffff);
+    }
+
+    private function p0($x): int
+    {
+        return $x ^ $this->lm($x, 9) ^ $this->lm($x, 17);
+    }
+
+    private function p1($x): int
+    {
+        return $x ^ $this->lm($x, 15) ^ $this->lm($x, 23);
     }
 }
---
+++ tests/unit/SM3Test.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace axios\tools\tests\unit;
+
+use axios\tools\HMac;
+use axios\tools\Path;
+use axios\tools\SM3;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class SM3Test extends TestCase
+{
+    private ?SM3 $sm3;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->sm3 = new SM3();
+    }
+
+    public function testEncodeWithEmptyString()
+    {
+        $this->sm3->encode('');
+        $this->assertEquals(hex2bin('1ab21d8355cfa17f8e61194831e81a8f22bec8c728fefb747ed035eb5082aa2b'), $this->sm3->getBinary());
+        $this->assertEquals('1ab21d8355cfa17f8e61194831e81a8f22bec8c728fefb747ed035eb5082aa2b', $this->sm3->getHex());
+        $this->assertEquals('GrIdg1XPoX+OYRlIMegajyK+yMco/vt0ftA161CCqis=', $this->sm3->getBase64());
+    }
+
+    public function testEncode()
+    {
+        $this->sm3->encode('test');
+        $this->assertEquals(hex2bin('55e12e91650d2fec56ec74e1d3e4ddbfce2ef3a65890c2a19ecf88a307e76a23'), $this->sm3->getBinary());
+        $this->assertEquals('55e12e91650d2fec56ec74e1d3e4ddbfce2ef3a65890c2a19ecf88a307e76a23', $this->sm3->getHex());
+        $this->assertEquals('VeEukWUNL+xW7HTh0+Tdv84u86ZYkMKhns+IowfnaiM=', $this->sm3->getBase64());
+    }
+
+    public function testEncodeWithFile()
+    {
+        $filepath = Path::join(__DIR__, '../../test.tmp');
+        $this->write($filepath, 'test', 'w');
+        $this->sm3->encodeFile($filepath);
+        $this->assertEquals(hex2bin('55e12e91650d2fec56ec74e1d3e4ddbfce2ef3a65890c2a19ecf88a307e76a23'), $this->sm3->getBinary());
+        $this->assertEquals('55e12e91650d2fec56ec74e1d3e4ddbfce2ef3a65890c2a19ecf88a307e76a23', $this->sm3->getHex());
+        $this->assertEquals('VeEukWUNL+xW7HTh0+Tdv84u86ZYkMKhns+IowfnaiM=', $this->sm3->getBase64());
+    }
+
+    public function testEncodeWithHMac()
+    {
+        $hmac = new HMac();
+        $hmac->registerAlgorithm('sm3', function ($str) {
+            $sm3 = new SM3();
+            $sm3->encode($str);
+
+            return $sm3->getHex();
+        });
+        $this->assertEquals(
+            '71e9db0344cd62427ccb824234214e14a0a54fe80adfb46bd12453270961dd5b',
+            $hmac->count('sm3', '', 'secret')
+        );
+        $this->assertEquals(
+            hex2bin('71e9db0344cd62427ccb824234214e14a0a54fe80adfb46bd12453270961dd5b'),
+            $hmac->count('sm3', '', 'secret', true)
+        );
+        $this->assertEquals(
+            '96042a28529e7a438af81eece5b293e0699f481fd372c08c5ac01b8dc4b81856',
+            $hmac->count('sm3', 'abc', 'secret')
+        );
+        $this->assertEquals(
+            hex2bin('96042a28529e7a438af81eece5b293e0699f481fd372c08c5ac01b8dc4b81856'),
+            $hmac->count('sm3', 'abc', 'secret', true)
+        );
+        $this->assertEquals(
+            '8e4bd77d8a10526fae772bb6014dfaed0335491e1cdfa92d3aca1481ae5d9a83',
+            $hmac->count('sm3', str_repeat('abc', 1000), 'secret')
+        );
+        $this->assertEquals(
+            hex2bin('8e4bd77d8a10526fae772bb6014dfaed0335491e1cdfa92d3aca1481ae5d9a83'),
+            $hmac->count('sm3', str_repeat('abc', 1000), 'secret', true)
+        );
+    }
+
+    private function write(string $filename, string $text, string $mode): void
+    {
+        if (!file_exists(\dirname($filename))) {
+            @mkdir(\dirname($filename), 0755, true);
+        }
+        $fp = fopen($filename, $mode);
+        if (flock($fp, LOCK_EX)) {
+            fwrite($fp, $text);
+            flock($fp, LOCK_UN);
+        }
+        fclose($fp);
+    }
+}
