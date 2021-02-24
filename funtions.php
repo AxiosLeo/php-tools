f4ace5eed8a44ca82a71ab2c7e79dc021a562662
axios <axiosleo@foxmail.com>
2021-02-24 22:01:39 +0800

    feat: add client_ip() function

--- funtions.php
+++ funtions.php
@@ -19,6 +19,47 @@ if (!function_exists('halt') && function_exists('dump')) {
     function halt(...$params)
     {
         dump(...$params);
-        die();
+
+        exit();
+    }
+}
+
+if (!function_exists('client_ip')) {
+    /*
+     * get the IP address of the client
+     *
+     * @param int  $type 0 => return IP string; 1=> return IP number
+     * @param bool $adv  advance mode
+     *
+     * @return mixed
+     */
+    function client_ip($type = 0, $adv = false)
+    {
+        $type              = $type ? 1 : 0;
+        static $ip         = null;
+        if (null !== $ip) {
+            return $ip[$type];
+        }
+        if ($adv) {
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+                $pos = array_search('unknown', $arr);
+                if (false !== $pos) {
+                    unset($arr[$pos]);
+                }
+                $ip = trim($arr[0]);
+            } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+                $ip = $_SERVER['REMOTE_ADDR'];
+            }
+        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        // IP address legitimacy verification
+        $long = sprintf('%u', ip2long($ip));
+        $ip   = $long ? [$ip, $long] : ['0.0.0.0', 0];
+
+        return $ip[$type];
     }
 }
